3112428a8d322d621f631a8575a852c2215ef4bb
jaebooker <jaesonbooker@gmail.com>
2020-02-01 15:44:34 -0800

    added map and dummy data

--- CryptoFinder/ViewController.swift
+++ CryptoFinder/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var coordinate = CLLocationCoordinate2D()
+    let regionInMeters: Double = 10000.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        guard let newCoordinates = locationManager.location?.coordinate else { return }
+        let region = MKCoordinateRegion(center: newCoordinates, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapView.setRegion(region, animated: true)
+        coordinate = newCoordinates
+        
+        let annotation1 = MKPointAnnotation()
+        annotation1.title = "Bulba Fett Tea"
+        annotation1.subtitle = "Everyone's favourite tea inspired by everyone's favourite Star Trek character"
+        annotation1.coordinate = coordinate
+        mapView.addAnnotation(annotation1)
+        
+        let annotation2 = MKPointAnnotation()
+        annotation2.title = "Kale"
+        annotation2.subtitle = "Kale French Fries!"
+        annotation2.coordinate = CLLocationCoordinate2D(latitude: 37.780664, longitude: -122.416183)
+        mapView.addAnnotation(annotation2)
+        
     }
 
-
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+        pin.canShowCallout = true
+        pin.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        return pin
+    }
+    
 }
 
