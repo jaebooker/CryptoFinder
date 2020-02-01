6ef83185a0c9a7acac408df771460dda14ef1ee0
jaebooker <jaesonbooker@gmail.com>
2020-02-01 12:39:18 -0800

    Initial Commit

---
+++ CryptoFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoFinder
+//
+//  Created by Jaeson Booker on 2/1/20.
+//  Copyright © 2020 Jaeson Booker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
