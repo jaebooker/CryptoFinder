bd22b7ce08d60a691bee781daa8a68ab5200a3ff
jaebooker <jaesonbooker@gmail.com>
2020-02-02 00:15:40 -0800

    added api and crypto vendors

---
+++ CryptoFinder/Venue.swift
@@ -0,0 +1,36 @@
+//
+//  Venue.swift
+//  CryptoFinder
+//
+//  Created by Jaeson Booker on 2/1/20.
+//  Copyright © 2020 Jaeson Booker. All rights reserved.
+//
+
+import Foundation
+
+struct VenueList: Decodable {
+    let venues: [Venue]
+}
+//venue features matching API
+
+struct Venue: Decodable {
+    
+    let created_on: Int
+    let id: Int
+    let name: String
+    let category: String?
+    let lat: Float
+    let lon: Float
+    
+    let lat2: Float?
+    let lon1: Float?
+    let lon2: Float?
+    let query: String?
+    let owner: String?
+    let upvoter: String?
+    let before: Date?
+    let after: Date?
+    let offset: Int?
+    let mode: String?
+    
+}
--- CryptoFinder/ViewController.swift
+++ CryptoFinder/ViewController.swift
@@ -15,46 +15,34 @@ class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     private let locationManager = CLLocationManager()
     var coordinate = CLLocationCoordinate2D()
+    var venues: [Venue] = []
     var checkLocation: Bool = false
-    //let regionInMeters: Double = 10000.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocation = false
+        guard let url = URL(string: "https://coinmap.org/api/v1/venues/") else { return }
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            do {
+                let apiEvents = try JSONDecoder().decode(VenueList.self, from: data)
+                //looping through decoded venues
+                for apiEvent in apiEvents.venues {
+                    self.venues.append(apiEvent)
+                }
+                self.addAnnoations()
+            } catch {}
+        }
+        task.resume()
         configureLocationServices()
         
         mapView.delegate = self
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        guard let newCoordinates = locationManager.location?.coordinate else { return }
-//        let region = MKCoordinateRegion(center: newCoordinates, latitudinalMeters: 500, longitudinalMeters: 500)
-//        mapView.setRegion(region, animated: true)
-//        coordinate = newCoordinates
-//
-//        let annotation1 = MKPointAnnotation()
-//        annotation1.title = "Bulba Fett Tea"
-//        annotation1.subtitle = "Everyone's favourite tea inspired by everyone's favourite Star Trek character"
-//        annotation1.coordinate = coordinate
-//        mapView.addAnnotation(annotation1)
-//
-//        let annotation2 = MKPointAnnotation()
-//        annotation2.title = "Kale"
-//        annotation2.subtitle = "Kale French Fries!"
-//        annotation2.coordinate = CLLocationCoordinate2D(latitude: 37.780664, longitude: -122.416183)
-//        mapView.addAnnotation(annotation2)
         
     }
-
-//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-//        if annotation is MKUserLocation {
-//            return nil
-//        }
-//        let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
-//        pin.canShowCallout = true
-//        pin.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-//        return pin
-//    }
     
     private func configureLocationServices(){
         locationManager.delegate = self
@@ -74,25 +62,19 @@ class ViewController: UIViewController, MKMapViewDelegate {
     }
     
     private func zoomToLocation(coordinate: CLLocationCoordinate2D){
-        let region = MKCoordinateRegion(center: coordinate,latitudinalMeters: 9000,longitudinalMeters: 15000)
+        let region = MKCoordinateRegion(center: coordinate,latitudinalMeters: 5000,longitudinalMeters: 11000)
         mapView.setRegion(region, animated: true)
     }
     
+    
     private func addAnnoations(){
         
-        let testAnnotation = MKPointAnnotation()
-        testAnnotation.title = "Bulba Fett Tea"
-        testAnnotation.subtitle = "Everyone's favourite tea inspired by everyone's favourite Star Trek character"
-        testAnnotation.coordinate = coordinate
-        
-        let testAnnotation2 = MKPointAnnotation()
-        testAnnotation2.title = "Kale"
-        testAnnotation2.subtitle = "Kale French Fries!"
-        testAnnotation2.coordinate = CLLocationCoordinate2D(latitude: 37.780664, longitude: -122.416183)
-        
-        mapView.addAnnotation(testAnnotation)
-        mapView.addAnnotation(testAnnotation2)
-        
+        for venue in self.venues {
+            let newAnnoation = MKPointAnnotation()
+            newAnnoation.coordinate = CLLocationCoordinate2D(latitude: Double(venue.lat), longitude: Double(venue.lon))
+            newAnnoation.title = venue.name
+            mapView.addAnnotation(newAnnoation)
+        }
     }
     
 }
@@ -103,7 +85,6 @@ extension ViewController: CLLocationManagerDelegate {
         coordinate = latestLocation.coordinate
         if checkLocation == false {
             zoomToLocation(coordinate: coordinate)
-            addAnnoations()
             checkLocation = true
         }
     }
